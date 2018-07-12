874253257d04ed8de54b8211e8b8f29e86578d35
Subratasd <subratasd33@gmail.com>
2018-07-12 19:56:24 +0600

    new update

---
+++ vendor/src/HelperServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'config/helper.php' => config_path('helper.php')
+        ], 'config');
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
